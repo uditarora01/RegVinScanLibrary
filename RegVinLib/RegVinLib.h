5823ad8d6f207e1f6d5bb2db69180020108b1abd
Udit Arora <udit.arora@adglobal360.com>
2020-04-28 18:23:01 +0530

    Initial Commit

---
+++ RegVinLib/RegVinLib.h
@@ -0,0 +1,19 @@
+//
+//  RegVinLib.h
+//  RegVinLib
+//
+//  Created by UDIT ARORA on 28/04/20.
+//  Copyright © 2020 UDIT ARORA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RegVinLib.
+FOUNDATION_EXPORT double RegVinLibVersionNumber;
+
+//! Project version string for RegVinLib.
+FOUNDATION_EXPORT const unsigned char RegVinLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegVinLib/PublicHeader.h>
+
+
