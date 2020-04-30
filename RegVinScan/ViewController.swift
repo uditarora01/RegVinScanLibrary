5bfa5e48254e09939f54afb2c4dd3d9c2ed03f28
Udit Arora <udit.arora@adglobal360.com>
2020-04-30 14:53:16 +0530

    first commit

---
+++ RegVinScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegVinScan
+//
+//  Created by UDIT ARORA on 30/04/20.
+//  Copyright © 2020 UDIT ARORA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
